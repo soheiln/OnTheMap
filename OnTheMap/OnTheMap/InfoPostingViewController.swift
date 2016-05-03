fd0e7cc5446f9e1d9ecd33341e5c2b67b28badba
Soheil Norouzi <soheil.nrz@gmail.com>
2016-05-03 16:58:19 -0700

    Add InfoPosting View elements

--- OnTheMap/OnTheMap/InfoPostingViewController.swift
+++ OnTheMap/OnTheMap/InfoPostingViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class InfoPostingViewController: UIViewController {
     
+    
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var mapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,9 @@ class InfoPostingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
     
 }
 
