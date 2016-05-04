9ba9dac56fcdedf5ac2ee76157d578c4687b5ac9
Soheil Norouzi <soheil.nrz@gmail.com>
2016-05-03 17:17:34 -0700

    Add InfoPostingSubmit View

---
+++ OnTheMap/InfoPostingSubmitViewController.swift
@@ -0,0 +1,36 @@
+//
+//  InfoPostingSubmitViewController.swift
+//  OnTheMap
+//
+//  Created by soheiln on 5/3/16.
+//  Copyright © 2016 soheiln. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class InfoPostingSubmitViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func cancelButtonPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+}
+
